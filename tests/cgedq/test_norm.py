cad4cfa26080dc97e20cdeddafb21a04356ff539
Adam Burke <at.burke@qut.edu.au>
2024-12-10 16:46:36 +1000

    Academician Reader in Waiting; Chair of the Palace of Universal Peace school

--- tests/cgedq/test_norm.py
+++ tests/cgedq/test_norm.py
@@ -24,11 +24,28 @@ class NormTest(unittest.TestCase):
         self.assertEqual(['priest','pri','violist'],
                           split_position("priestpriviolist",
                                          ['pri','priest']))
+        self.assertEqual(['violist','priest','ly'],
+                          split_position("priestlyviolist",
+                                         ['priest','violist']))
 
     def test_split_cc(self):
         self.assertEqual(['戶部務','賞戴花翎'],
                           split_position('賞戴花翎戶部務',
                                          ['戶部務','正主考']))
+
+    def test_split_overlap_riw(self):
+        self.assertEqual(['翰林院侍讀學士','咸安宮總裁教習'],
+                      split_position('翰林院侍讀學士咸安宮總裁教習',
+                                    ['翰林院侍讀學士','咸安宮總裁教習']) )
+        self.assertEqual(['翰林院侍讀學士','咸安宮總裁教習'],
+                      split_position('翰林院侍讀學士咸安宮總裁教習',
+                                    ['翰林院侍讀學士','翰林院侍讀',
+                                     '咸安宮總裁教習']) )
+        self.assertEqual(['翰林院侍讀學士','咸安宮總裁教習'],
+                      split_position('翰林院侍讀學士咸安宮總裁教習',
+                                    ['翰林院侍讀','翰林院侍讀學士',
+                                     '咸安宮總裁教習']) )
+
 
     def test_normalize_position_empty(self):
         df = pd.DataFrame({'synjob' : ['Job 1', 'Job 2', 'Job 3']})
