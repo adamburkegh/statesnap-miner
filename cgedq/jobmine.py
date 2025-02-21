5cb84353c1030dd5687b65127ff300dff3f48c34
Adam Burke <at.burke@qut.edu.au>
2025-02-21 17:49:51 +1000

    Extract which doesn't assume graduate / TML join

--- cgedq/jobmine.py
+++ cgedq/jobmine.py
@@ -21,10 +21,11 @@ def mine_export_job(job,tag,jevents,tmlrec,officials,positions,appointments,
     info(f"Merged columns {list(events.columns.values)}")
     sjevents = events[events['synjob'] == job].copy()
     jpeople = sjevents['person_id'].unique()
+    info(f'Found {len(jpeople)} officials with job {job}')
     if len(sjevents) == 0:
-        print('No events')
+        info('No events')
         return
-    print(f'Found {len(sjevents)} events')
+    info(f'Found {len(sjevents)} events')
     events = events[events['person_id'].isin(jpeople) ]
     # officials = officials[officials.person_id.isin(jpeople)].copy()
     rt = '(No translation)'
@@ -44,6 +45,7 @@ def mine_export_job(job,tag,jevents,tmlrec,officials,positions,appointments,
 
 
 
+
 def mine_by_job(job,tag,noise,fin,rebuild_db,tmlin,inputtype,datadir,topn=10):
     info(f"Started at {datetime.now()}")
     ds = load_datasets(fin,rebuild_db,tmlin,inputtype,datadir)
@@ -52,17 +54,44 @@ def mine_by_job(job,tag,noise,fin,rebuild_db,tmlin,inputtype,datadir,topn=10):
     info(f"Finished at {datetime.now()}")
 
 
+
+
+# Basic extract
+def extract_by_job(job,tag,fin,inputtype,datadir):
+    info(f"Started at {datetime.now()}")
+    ds = load_datasets(fin,False,None,inputtype,datadir)
+    sjevents = ds.events[ds.events['synjob'].str.contains(job,na=False)].copy()
+    jpeople  = sjevents['person_id'].unique()
+    info(f'Found {len(jpeople)} officials with job {job}')
+    if len(sjevents) == 0:
+        info('No events')
+        return
+    info(f'Found {len(sjevents)} events')
+    events = sjevents[sjevents['person_id'].isin(jpeople) ]
+    # officials = officials[officials.person_id.isin(jpeople)].copy()
+    rt = '(No translation)'
+    jinit = selectfrominit(events, 
+                            "Officials who served as " + job + " : {}" )
+    if len(jinit) == 0:
+        print( f'No events after filtering for mid-career officials' )
+        return
+    extract_norm_events(jinit,tag,
+                        ds.officials,ds.positions,ds.appointments )
+
+
 def main():
     logging.basicConfig(level=logging.INFO)
     args = main_parse()
-    mine_by_job('知縣','jmagistrate', 0.0005, args.cgedqfile,args.rebuild,
-                args.tmlfile, args.inputtype, args.datadir,topn=12)
+    # mine_by_job('知縣','jmagistrate', 0.0005, args.cgedqfile,args.rebuild,
+    #            args.tmlfile, args.inputtype, args.datadir,topn=12)
     #mine_by_job('知縣','jmagistrate', 0.001, args.cgedqfile,args.rebuild,
     #            args.tmlfile, args.inputtype, args.datadir)
     # mine_by_job('文淵閣校理','jprofound', 0.002, args.cgedqfile,args.rebuild,
     #            args.tmlfile, args.inputtype, args.datadir)
     # mine_by_job('中允','jcomp', 0.002, args.cgedqfile,args.rebuild,
     #             args.tmlfile, args.inputtype, args.datadir)
+    extract_by_job('知縣','jmagfull', args.cgedqfile, args.inputtype, 
+                   args.datadir)
 
 
 if __name__ == "__main__":
